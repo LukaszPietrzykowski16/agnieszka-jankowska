7e0e1448feb3482586800d7989cf1f47fa19b791
unknown <lukas@DESKTOP-JE2377S>
2022-02-10 10:48:07 +0100

    i added some sections

--- src/Galeria.js
+++ src/Galeria.js
@@ -1,51 +1,97 @@
 import React, {useState, useEffect} from 'react';
+import img1 from './images/IMG_9425a.jpg';
+import img2 from './images/portfolio/IMG_6122.jpg';
+import img3 from './images/portfolio/IMG_2887.jpg';
+import img4 from './images/portfolio/7.jpg';
+import img5 from './images/portfolio/0138.jpg';
+import img6 from './images/portfolio/13.jpg';
+import img7 from './images/portfolio/IMG_6122.jpg';
+import img8 from './images/portfolio/IMG_2887.jpg';
+import img9 from './images/portfolio/IMG_7111.jpg';
+import img10 from './images/portfolio/IMG_7111.jpg';
 
 export default function Galeria() {
-  return (<div className='bruh'>
+  return <>  (
+  <div className='test-gallery'>
+    <div className='test-gallery-subsection'>
+      <div className='test-gallery-subsection-img'>
+        <img src={img2}/>
+      </div>
+      <div className='test-gallery-subsection-txt'>
+    
+        <p> 
+        <h1> Reportaż ślubny </h1>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras et ex quis eros viverra rutrum. Nulla imperdiet ante sit amet ultrices iaculis. Praesent ullamcorper mi eu risus viverra ultricies. Curabitur volutpat ultrices mauris eget fringilla. Praesent mollis semper congue. </p>
+      </div>
+    </div>
 
+    <div className='test-gallery-subsection'>
+
+    <div className='test-gallery-subsection-img'>
+        <img src={img2}/>
+      </div>
+    <div className='test-gallery-subsection-txt'>
+    
+    <p> 
+    <h1> Reportaż ślubny </h1>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras et ex quis eros viverra rutrum. Nulla imperdiet ante sit amet ultrices iaculis. Praesent ullamcorper mi eu risus viverra ultricies. Curabitur volutpat ultrices mauris eget fringilla. Praesent mollis semper congue. </p>
+  </div>
+      
+      
+     
+      
+    </div>
+
+    <div className='test-gallery-subsection'>
+      <div className='test-gallery-subsection-img'>
+        <img src={img2}/>
+      </div>
+      <div className='test-gallery-subsection-txt'>
+    
+        <p> 
+        <h1> Reportaż ślubny </h1>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras et ex quis eros viverra rutrum. Nulla imperdiet ante sit amet ultrices iaculis. Praesent ullamcorper mi eu risus viverra ultricies. Curabitur volutpat ultrices mauris eget fringilla. Praesent mollis semper congue. </p>
+      </div>
+    </div>
+
+    <div className='test-gallery-subsection'>
+      <div className='test-gallery-subsection-img'>
+        <img src={img2}/>
+      </div>
+      <div className='test-gallery-subsection-txt'>
+    
+        <p> 
+        <h1> Reportaż ślubny </h1>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras et ex quis eros viverra rutrum. Nulla imperdiet ante sit amet ultrices iaculis. Praesent ullamcorper mi eu risus viverra ultricies. Curabitur volutpat ultrices mauris eget fringilla. Praesent mollis semper congue. </p>
+      </div>
+    </div>
+
+    <div className='test-gallery-subsection'>
+      <div className='test-gallery-subsection-img'>
+        <img src={img2}/>
+      </div>
+      <div className='test-gallery-subsection-txt'>
+    
+        <p> 
+        <h1> Reportaż ślubny </h1>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras et ex quis eros viverra rutrum. Nulla imperdiet ante sit amet ultrices iaculis. Praesent ullamcorper mi eu risus viverra ultricies. Curabitur volutpat ultrices mauris eget fringilla. Praesent mollis semper congue. </p>
+      </div>
+    </div>
+
+    <div className='test-gallery-subsection'>
+      <div className='test-gallery-subsection-img'>
+        <img src={img2}/>
+      </div>
+      <div className='test-gallery-subsection-txt'>
+    
+        <p> 
+        <h1> Reportaż ślubny </h1>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras et ex quis eros viverra rutrum. Nulla imperdiet ante sit amet ultrices iaculis. Praesent ullamcorper mi eu risus viverra ultricies. Curabitur volutpat ultrices mauris eget fringilla. Praesent mollis semper congue. </p>
+      </div>
+    </div>
+    
   </div>)
-  /*
-  const [error, setError] = useState(null);
-  const [isLoaded, setIsLoaded] = useState(false);
-  const [items, setItems] = useState([]);
-
-  // Note: the empty deps array [] means
-  // this useEffect will run once
-  // similar to componentDidMount()
-  useEffect(() => {
-    fetch("https://drive.google.com/drive/folders/17fLbIxvzDnU1mlhV04APwcG06wyxH74N")
-      .then(res => res.json())
-      .then(
-        (result) => {
-          setIsLoaded(true);
-          setItems(result);
-        },
-        // Note: it's important to handle errors here
-        // instead of a catch() block so that we don't swallow
-        // exceptions from actual bugs in components.
-        (error) => {
-          setIsLoaded(true);
-          setError(error);
-        }
-      )
-  }, [])
-
-  if (error) {
-    return <div>Error: {error.message}</div>;
-  } else if (!isLoaded) {
-    return <div>Loading...</div>;
-  } else {
-    return (
-      <ul>
-        {items.map(item => (
-          <li key={item.id}>
-            {item.name} {item.price}
-          </li>
-        ))}
-      </ul>
-    );
-  }
-  */
+  </>
 }
 
 
